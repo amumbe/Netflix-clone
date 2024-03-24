cc74c22086ec2938e9490214618463de6933de88
amumbe <angellamumbe720@gmail.com>
2024-03-24 20:26:42 +0300

    -m

--- netflixapp/models.py
+++ netflixapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
 
 # Create your models here.
+
+AGE_LIMIT =(
+    ('All', 'All'),
+    ('Kids', 'Kids'),
+)
+
+class CustomUser(AbstractUser):
+    profiles = models.models.ManyToManyField("Profile", blank = True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length = 1000)
+    age_limit =
