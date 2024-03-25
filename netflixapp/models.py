7da4cbaf8e6c0416970aed6831f8340376ec467b
amumbe <angellamumbe720@gmail.com>
2024-03-25 22:30:24 +0300

    -m

--- netflixapp/models.py
+++ netflixapp/models.py
@@ -5,15 +5,31 @@ import uuid
 
 # Create your models here.
 
-AGE_LIMIT =(
-    ('All', 'All'),
-    ('Kids', 'Kids'),
+AGE_LIMIT = (
+    ("All", "All"),
+    ("Kids", "Kids"),
 )
 
+MOVIE_CHOICES =(
+    ('seasonal', 'Seasonal'),
+    ('single', 'Single'),
+)
+
+
 class CustomUser(AbstractUser):
-    profiles = models.models.ManyToManyField("Profile", blank = True)
+    profiles = models.models.ManyToManyField("Profile", blank=True)
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length = 1000)
-    age_limit =
+    name = models.CharField(max_length=1000)
+    age_limit = models.CharField(choices=AGE_LIMIT, max_length=10)
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField(blank = True, null = True)
+    created = models.DateTimeField(auto_now_add = True)
+    uuid = models.UUIDField(default= uuid.uuid4)
+    type = models.CharField(choices = MOVIE_CHOICES, max_length = 100 )
+
