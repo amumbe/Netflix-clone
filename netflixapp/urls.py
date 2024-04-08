2c31f6faf404cc8a4829fce609650a76e9c1702e
amumbe <angellamumbe720@gmail.com>
2024-04-08 22:38:52 +0300

    -m

--- netflixapp/urls.py
+++ netflixapp/urls.py
@@ -3,4 +3,4 @@ from .views import Home
 
 app_name = "netflixapp"
 
-urlpatterns = [path("", Home.as_view(), name="home")]
+urlpatterns = [path("", Home.as_view(), name="Home")]
--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -136,7 +136,7 @@ AUTH_USER_MODEL = "netflixapp.CustomUser"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'netflixapp.Home'
 # LOGIN_URL = 'login'
 
 
